0fdc26b478c5a156e8f790a219555007114302ba
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-07-20 14:36:27 +0100

    Projet de plateforme de parful

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // fichiers à scanner pour classes Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
